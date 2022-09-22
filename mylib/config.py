efdbc2dd26adcacf957fc66dfa67bf963378a952
sudharsandev <107601853+sudharsandev@users.noreply.github.com>
2022-09-22 16:20:25 +0530

    Add files via upload

---
+++ mylib/config.py
@@ -0,0 +1,16 @@
+""" Optional features config. """
+MAIL = '192125089.sse@saveetha.com'
+# Enter the ip camera url 
+url = '0'
+# ON/OFF for mail feature. 
+ALERT = False
+# Set max. people inside limit. Optimise number below: 10, 50, 100.
+Threshold = 2
+# Threading ON/OFF
+Thread = False
+# Simple log to log the counting data
+Log = True
+# Auto run/Schedule the software to run at your desired time
+Scheduler = False
+# Auto stop the software after certain a time/hours
+Timer = False
